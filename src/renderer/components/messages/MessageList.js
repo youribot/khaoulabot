9b48308be112494503e7b8fdfee1372485e1cb26
sibizulu <sibiraj@lightrains.com>
2017-01-18 13:58:57 +0530

    Adding component folder

---
+++ src/renderer/components/messages/MessageList.js
@@ -0,0 +1,27 @@
+import React, { PropTypes } from 'react'
+import MessageRow from './MessageRow'
+import { pacomoTransformer } from '../utils/pacomo'
+
+
+const MessageList = props => {
+
+  let messages = {}
+  if(props.data && props.data.length > 0)
+    messages = props.data.map((item, index) => {
+      return <MessageRow key = {index} id = {index} message = {item.message} position = {item.position} icon = {item.icon}/>
+    })
+  else
+    messages = <div>Lets start a conversation</div>
+  return (
+    <div id = {'chatbox'} >
+      <div id = {'chat-messageboxs'} className = {'animate'} >{messages}</div>
+    </div>
+  )
+
+}
+
+MessageList.propTypes = {
+  data: PropTypes.array
+}
+
+export default pacomoTransformer(MessageList)
