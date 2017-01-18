2ed1f9fb7dec90c5fa29014a0a4ec883d05d2c86
sibizulu <sibiraj@lightrains.com>
2017-01-18 17:19:54 +0530

    Adding dummy chat

--- src/renderer/containers/BotContainer.js
+++ src/renderer/containers/BotContainer.js
@@ -1,20 +1,26 @@
 import React, { PropTypes, Component } from 'react'
 import { connect } from 'react-redux'
+import { reset } from 'redux-form'
 
 import MessageList from '../components/messages/MessageList'
 import MessageSend from '../components/messages/MessageSend'
 
+import { addLine, inputClickHandler } from '../actions/myaction'
 
-export const inputClickHandler = (line, dispatch) => {
-  dispatch({type: 'QUESTION_MADE', line: line.chatMessage})
-  dispatch(reset('chatbot'))
-}
+import { getLines } from '../reducers/myreducer'
+
+
+let dataArray = [];
+dataArray[0] = {message : "Hai there?","icon" : "../../assets/youribot.png","position" : "left"};
+dataArray[1] = {message : "Yes Here!!!","icon" : "../../assets/user_icon.png","position" : "right"};
+dataArray[3] = {message : "What can i do for you?","icon" : "../../assets/youribot.png","position" : "left"};
+
 
 const BotContainer = props => {
 
   return (
     <div id = {'chatbox'}>
-      <MessageList data = {props.statusdata} />
+      <MessageList data = {dataArray} />
       <MessageSend onSubmit = {inputClickHandler.bind(this)} />
     </div>
   )
@@ -22,10 +28,16 @@ const BotContainer = props => {
 }
 
 
+const mapStateToProps = state => {
+  return {
+    statusdata: getLines(state)
+  }
+}
+
 BotContainer.propTypes = {
   statusdata: PropTypes.array,
   //dispatch: PropTypes.func.isRequired,
 }
 
 
-export default BotContainer
+export default connect(mapStateToProps)(BotContainer)
