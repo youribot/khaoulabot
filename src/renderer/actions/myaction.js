94c728ef92b0b88a22bb6b6e0bfa87f745806d9d
sibizulu <sibiraj@lightrains.com>
2017-01-19 14:41:42 +0530

    Removing fun

--- src/renderer/actions/myaction.js
+++ src/renderer/actions/myaction.js
@@ -2,13 +2,7 @@ import { reset } from 'redux-form'
 
 export const ADD_LINE = 'ADD_LINE'
 export const ADD_LINES = 'ADD_LINES'
-export const QUESTION_MADE = 'QUESTION_MADE'
-export const RECORD_STEP = 'RECORD_STEP'
 export const ADD_LINE_BY_USER = 'ADD_LINE_BY_USER'
-export const ADD_ORDER_ITEM = 'ADD_ORDER_ITEM'
-export const ADD_ORDER_ITEMS = 'ADD_ORDER_ITEMS'
-export const REMOVE_ORDER_ITEM = 'REMOVE_ORDER_ITEM'
-export const INC_ORDER_ITEM = 'INC_ORDER_ITEM'
 
 export const addLine = line => {
 
@@ -28,43 +22,7 @@ export const addLines = lines => {
 
 }
 
-export const addOrderItems = items => {
-
-  return {
-    type: ADD_ORDER_ITEMS,
-    items
-  }
-
-}
-
-export const addOrderItem = item => {
-
-  return {
-    type: ADD_ORDER_ITEM,
-    item
-  }
-
-}
-
-export const incOrderItem = index => {
-
-  return {
-    type: INC_ORDER_ITEM,
-    index
-  }
-
-}
-
-export const removeOrderItem = item => {
-
-  return {
-    type: REMOVE_ORDER_ITEM,
-    item
-  }
-
-}
-
-export const addLineByUser= line => {
+export const addLineByUser = line => {
 
   return {
     type: ADD_LINE_BY_USER,
@@ -73,25 +31,10 @@ export const addLineByUser= line => {
 
 }
 
-export const recordStep = step => {
-
-  return {
-    type: RECORD_STEP,
-    step
-  }
-
-}
-
-export function questionMade() {
-
-  return {
-    type: QUESTION_MADE
-  }
-
-}
 
 export const inputClickHandler = (line, dispatch) => {
-  dispatch({type: 'QUESTION_MADE', line: line.chatmessage})
+
+  dispatch({type: 'ADD_LINE', line: "testing"})
   // Reset form
-  dispatch(reset('chatbot'))
+  //dispatch(reset('chatbot'))
 }
