db58ca2f07cd1e598e5db383d92b3ddd86d31afe
sibizulu <sibiraj@lightrains.com>
2017-01-19 13:43:05 +0530

    Adding no message

--- src/renderer/components/messages/MessageList.js
+++ src/renderer/components/messages/MessageList.js
@@ -7,10 +7,10 @@ const MessageList = props => {
   let messages = {}
   if(props.data && props.data.length > 0)
     messages = props.data.map((item, index) => {
-      return <MessageRow key = {index} id = {index} message = {item.message} position = {item.position} icon = {item.icon}/>
+      return <MessageRow key = {index} id = {index} message = {item.message} position = {item.position} />
     })
   else
-    messages = <div>Lets start a conversation</div>
+    messages = <MessageRow key = {0} id = {0} message = {'Lorem ipsum dolor sit amet, consectetur adipisicing elit!'} position = {'left'} />
   return (
       <div id = {'chat-messageboxs'} className = {'animate'} >{messages}</div>
   )
