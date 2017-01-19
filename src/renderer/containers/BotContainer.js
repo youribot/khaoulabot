05e270eb604d875d0b8a1107247c92526de384cf
sibizulu <sibiraj@lightrains.com>
2017-01-19 13:41:42 +0530

    Remove dummy data

--- src/renderer/containers/BotContainer.js
+++ src/renderer/containers/BotContainer.js
@@ -10,17 +10,11 @@ import { addLine, inputClickHandler } from '../actions/myaction'
 import { getLines } from '../reducers/myreducer'
 
 
-let dataArray = [];
-dataArray[0] = {message : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.","icon" : "../../assets/youribot.png","position" : "left"};
-dataArray[1] = {message : "Yes Here!!!","icon" : "../../assets/user_icon.png","position" : "right"};
-dataArray[3] = {message : "What can i do for you?","icon" : "../../assets/youribot.png","position" : "left"};
-
-
 const BotContainer = props => {
 
   return (
     <div id = {'chatbox'}>
-      <MessageList data = {dataArray} />
+      <MessageList data = {props.statusdata} />
       <MessageSend onSubmit = {inputClickHandler.bind(this)} />
     </div>
   )
