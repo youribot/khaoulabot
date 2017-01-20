cd8c03a9324c3cb8d47865f72e1f62b8a86b245c
sibizulu <sibiraj@lightrains.com>
2017-01-20 17:05:31 +0530

    Adding status reducer

---
+++ src/renderer/redux/modules/statusReducer.js
@@ -0,0 +1,28 @@
+const initialState =  []
+
+const statusReducer = (state = initialState, action) => {
+
+  switch(action.type) {
+
+    case 'ADD_LINES':
+      let lines = action.lines
+      let newLines = []
+      lines.map(line => {
+        newLines.push({line, actor: 10})
+      })
+      return state.concat(newLines)
+
+    case 'ADD_LINE':
+      return state.concat({line: action.line, actor: 0})
+
+    case 'ADD_LINE_BY_USER':
+      return state.concat({line: action.line, actor: 1})
+
+    default:
+      return state
+  }
+}
+
+export const getLines = state => state.mystatus
+
+export default statusReducer
