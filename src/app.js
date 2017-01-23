b8ec5d948eb8ae95ea0dbc052a1375b216bcb62c
niksmac <hello@nikhilmohan.in>
2017-01-23 14:55:47 +0530

    Linted

--- src/app.js
+++ src/app.js
@@ -1,38 +1,38 @@
-'use strict';
-import {app, BrowserWindow} from 'electron';
+'use strict'
+import {app, BrowserWindow} from 'electron'
 
 import installExtension, {
   REACT_DEVELOPER_TOOLS,
-  REDUX_DEVTOOLS, REACT_PERF,
-} from 'electron-devtools-installer';
+  REDUX_DEVTOOLS, REACT_PERF
+} from 'electron-devtools-installer'
 
 installExtension('lmhkpmbekcpmknklioeibfkpmmfibljd')
 
-const Hapi = require('hapi');
-const server = new Hapi.Server();
+const Hapi = require('hapi')
+const server = new Hapi.Server()
 
 server.connection({
   host: 'localhost',
   port: 8000
-});
-let mainWindow = null;
+})
 
+let mainWindow = null
 app.on('window-all-closed', () => {
-  app.quit();
-});
+  app.quit()
+})
 
 app.on('ready', () => {
   server.register({
     register: require('hapi-routes'),
-    options: {dir: 'src/routes/'}
+    options: {dir: 'src/routes'}
   }, function (err) {
     server.start((err) => {
       if (err) {
-        throw err;
+        throw err
       }
-      console.log('Server running at:', server.info.uri);
-    });
-  });
+      console.log('Server running at:', server.info.uri)
+    })
+  })
 
   mainWindow = new BrowserWindow({
     width: 700,
@@ -40,9 +40,8 @@ app.on('ready', () => {
     minWidth: 360,
     minHeight: 500,
     titleBarStyle: 'hidden-inset'
-  });
+  })
 
-  mainWindow.loadURL(`file://${__dirname}/renderer/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/renderer/index.html`)
   mainWindow.webContents.openDevTools()
-
-});
+})
