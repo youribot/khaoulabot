706e57b1ca630d4e5c7c27d76d79c10d95f8ebc6
niksmac <hello@nikhilmohan.in>
2017-01-24 08:46:47 +0530

    Linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,46 @@
-const gulp = require('gulp');
-var gulpCopy = require('gulp-copy');
-var electron = require('gulp-electron');
-var packageJson = require('./package.json');
+const gulp = require('gulp')
+var gulpCopy = require('gulp-copy')
+var electron = require('gulp-electron')
+var packageJson = require('./package.json')
 
 // Copying semantic-ui to src.
-var sourceFiles = [ 'semantic/dist/**/*' ];
-var destination = 'src/renderer/styles/';
-gulp.task('copy-assets', function() {
+var sourceFiles = [ 'semantic/dist/**/*' ]
+var destination = 'src/renderer/styles/'
+gulp.task('copy-assets', function () {
   return gulp
     .src(sourceFiles)
     .pipe(gulp.dest(destination))
-});
+})
 
-gulp.task('electron', function() {
-
-    gulp.src("")
+gulp.task('electron', function () {
+  gulp.src('')
     .pipe(electron({
-        src: './src',
-        packageJson: packageJson,
-        release: './release',
-        cache: './cache',
-        version: 'v1.4.14',
-        packaging: true,
+      src: './src',
+      packageJson: packageJson,
+      release: './release',
+      cache: './cache',
+      version: 'v1.4.14',
+      packaging: true,
         // platforms: ['win32-ia32', 'darwin-x64'],
-        platforms: ['darwin-x64'],
-        platformResources: {
-            darwin: {
-                CFBundleDisplayName: packageJson.name,
-                CFBundleIdentifier: packageJson.name,
-                CFBundleName: packageJson.name,
-                CFBundleVersion: packageJson.version,
-                icon: 'assets/youribot.icns'
-            }
+      platforms: ['darwin-x64'],
+      platformResources: {
+        darwin: {
+          CFBundleDisplayName: packageJson.name,
+          CFBundleIdentifier: packageJson.name,
+          CFBundleName: packageJson.name,
+          CFBundleVersion: packageJson.version,
+          icon: 'assets/youribot.icns'
+        }
             // win: {
             //     "version-string": packageJson.version,
             //     "file-version": packageJson.version,
             //     "product-version": packageJson.version,
             //     "icon": 'assets/logo.png.ico'
             // }
-        }
+      }
     }))
-    .pipe(gulp.dest(""));
-});
-
+    .pipe(gulp.dest(''))
+})
 
 // Default Task
-gulp.task('default', ['copy-assets']);
+gulp.task('default', ['copy-assets'])
