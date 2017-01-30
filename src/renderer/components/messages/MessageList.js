b73ce6dd0b28b2a417f937562b8087104d8a8436
sibizulu <sibiraj@lightrains.com>
2017-01-30 13:02:29 +0530

    MessageList add class component

--- src/renderer/components/messages/MessageList.js
+++ src/renderer/components/messages/MessageList.js
@@ -2,30 +2,38 @@ import React, { PropTypes } from 'react'
 import functional from 'react-functional'
 import MessageRow from './MessageRow'
 
-const MessageList = props => {
+class MessageList extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
   let messages = {}
-  if (props.data && props.data.length > 0) {
-    messages = props.data.map((item, index) => {
+  if (this.props.data && this.props.data.length > 0) {
+    messages = this.props.data.map((item, index) => {
       return <MessageRow
-        key={index}
-        id={index}
-        message={item.line}
-        position={item.position}
-        identity={item.actor} />
+               key={index}
+               id={index}
+               message={item.line}
+               position={item.position}
+               identity={item.actor} />
     })
   } else {
     messages = <MessageRow
-      key={0}
-      id={0}
-      message={'What can i do for you?'}
-      position={'left'} />
+                 key={0}
+                 id={0}
+                 message={'What can i do for you?'}
+                 position={'left'} />
   }
-  return (
-    <div id={'chat-messageboxs'} className={'animate'}>
-      {messages}
-    </div>
-  )
+
+  render () {
+    return (
+      <div id={'chat-messageboxs'} className={'animate'}>
+        {messages}
+      </div>
+    )
+  }
 }
+
 MessageList.propTypes = {
   data: PropTypes.array
 }
