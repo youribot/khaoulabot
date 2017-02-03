5731d417c7231bba525d6b22809e866944b098a5
sibizulu <sibiraj@lightrains.com>
2017-02-03 14:47:35 +0530

    Adding intractive action

--- src/renderer/components/messages/MessageBeat.js
+++ src/renderer/components/messages/MessageBeat.js
@@ -7,4 +7,25 @@ const MessageBeat = props => {
     </div>
   )
 }
+
+const Form = ({ isSubmitted, buttonText }) => {
+  if (isSubmitted) {
+    // Form submitted! Return a message element.
+    return {
+      type: Message,
+      props: {
+        text: 'Success!'
+      }
+    }
+  }
+
+  // Form is still visible! Return a button element.
+  return {
+    type: Button,
+    props: {
+      children: buttonText,
+      color: 'blue'
+    }
+  }
+}
 export default MessageBeat
