2c1801fb58dd1d1557f6a67f8ef910bd3f5271ad
sibizulu <sibiraj@lightrains.com>
2017-02-06 10:34:41 +0530

    Adding hide show chatbox

--- src/renderer/components/messages/MessageSend.js
+++ src/renderer/components/messages/MessageSend.js
@@ -2,12 +2,14 @@ import React, { PropTypes, Component } from 'react'
 import { SubmissionError, Field, reduxForm } from 'redux-form'
 import { Form, Label, Input } from 'semantic-ui-react'
 import { inputClickHandler } from '../../actions/myaction'
+import ToggleDisplay from 'react-toggle-display'
 
 class MessageInput extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      chatmessage: ''
+      chatmessage: '',
+      isShown: false
     }
   }
 
@@ -21,6 +23,10 @@ class MessageInput extends React.Component {
     this.setState({chatmessage: ''})
   }
 
+  handleToggleClick (e) {
+    this.setState({ isShown: !this.state.isShown })
+  }
+
   renderInput (field) {
     return <div>
              <input {...field.input} type={field.type} placeholder={field.placeholder} />
@@ -30,23 +36,35 @@ class MessageInput extends React.Component {
   render () {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))} onChange={this.onChange.bind(this)}>
-          <div id={'sendmessagebox'}>
-            <Field
-              name={'chatmessage'}
-              component={this.renderInput}
-              type={'text'}
-              placeholder={'Send message...'}
-              value={this.state.chatmessage}
-              className={'ui input fluid'} />
-            <button
-              id={'send'}
-              className={'ui icon basic noborder large button'}
-              type={'submit'}
-              disabled={!this.state.chatmessage}>
-              <i className={'send icon'} />
-            </button>
+        <div id='infoSection' className='hidden'>
+          <div className='ui grid'>
+            <div className='ten wide column'>
+              <a className='ui right pointing label' onClick={this.handleToggleClick.bind(this)}>answer</a>
+            </div>
+            <div className='five wide column right aligned'>
+              <a data-actn='plus' className='ui black label'>+</a>
+            </div>
           </div>
+        </div>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))} onChange={this.onChange.bind(this)}>
+          <ToggleDisplay show={this.state.isShown}>
+            <div id={'sendmessagebox'}>
+              <Field
+                name={'chatmessage'}
+                component={this.renderInput}
+                type={'text'}
+                placeholder={'Send message...'}
+                value={this.state.chatmessage}
+                className={'ui input fluid'} />
+              <button
+                id={'send'}
+                className={'ui icon basic noborder large button'}
+                type={'submit'}
+                disabled={!this.state.chatmessage}>
+                <i className={'send icon'} />
+              </button>
+            </div>
+          </ToggleDisplay>
         </form>
       </div>
     )
